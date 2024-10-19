f79ff16fdd7f6288bf78aa736bf34fb43c157143
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-10-19 11:54:29 -0500

    Agregar query personalizados

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,7 @@
 const Query = {
-    hello: () => {return 'Hello World'}
+    hello: () => {return 'Hello World'},
+    quantity: () => 1,
+    getUser: () => { return { name: 'Jean Pool', lastName: 'Gutierrez' } }
 }
 
 export default Query
