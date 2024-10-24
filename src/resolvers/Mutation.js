a00ee9bf3ff48d85a99a9588a473046c7045f3f9
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-10-23 19:12:30 -0500

    Creando mutations para los libros

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -41,7 +41,35 @@ const Mutation = {
 
         return { ...userExist, ...data}
 
-    }
+    },
+    createAuthor: (parent, args, {db}, info) => {
+        const author = {
+            id: uuidv4(),
+            ...args,
+        }
+
+        db.authors.push(author)
+        return author
+    },
+    updateAuthor: (parent, args, {db}, info) => {
+        const { id, ...data } = args
+        const authorExist = db.authors.find(author => author.id === id)
+
+        if(!authorExist) {
+            throw new Error('Author does not exist')
+        }
+
+        db.authors = db.authors.map(author => {
+            if (author.id === id){
+                author = { ...author, ...data }
+                return author
+            }
+
+            return author
+        })
+
+        return { ...authorExist, ...data }
+    },
 }
 
 export default Mutation;
